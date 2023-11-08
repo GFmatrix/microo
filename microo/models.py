3f9812c19ea9335eaea9f54bb078a1ab1ac9088e
GFmatrix <sketchapps.lp@gmail.com>
2023-11-08 20:24:09 +0500

    First Commit

---
+++ microo/models.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.db import models
+
+# Create your models here.
+class Services(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='services')
+
+    def __str__(self):
+        return self.title
+
+class About(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='about')
+
+    def __str__(self):
+        return self.title
+      
+class Mics(models.Model):
+  image = models.ImageField(upload_to='mics')
+  
+class Comments(models.Model):
+  message = models.TextField()
+  
+  def __str__(self):
+    return self.name
+
+class Settings(models.Model):
+  address = models.CharField(max_length=100)
+  phone = models.CharField(max_length=100)
+  email = models.EmailField()
+  
+  facebook_link = models.CharField(max_length=100)
+  instagram_link = models.CharField(max_length=100)
+  twitter_link = models.CharField(max_length=100)
+
+class ReqCallBack(forms.Form):
+  name = forms.CharField(max_length=100)
+  email = forms.EmailField()
+  phone = forms.CharField(max_length=100)
+  message = forms.CharField(max_length=100)
+  
+  def __str__(self):
+    return self.name
